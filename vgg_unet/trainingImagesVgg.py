a6722eda57228523079a555d24f1f02b74dc8a60
Andressa <dessaamaralcunha@gmail.com>
2021-03-23 08:34:27 -0300

    Testar com menos épocas

--- vgg_unet/trainingImagesVgg.py
+++ vgg_unet/trainingImagesVgg.py
@@ -6,14 +6,14 @@ def main():
 
 def learning():
     n_classes = 24
-    epochs = 10
+    epochs = 5
     model = vgg_unet(n_classes=n_classes, input_height=416, input_width=608)
     model.train( 
         train_images =  "original_images/",
         train_annotations = "masks_images_semantic_result/",
         checkpoints_path = "vgg_unet" , epochs=epochs)
 
-    for i in range(120):
+    for i in range(60):
         input_image = "original_images/" + str(i) + ".jpg"
         out = model.predict_segmentation(
             inp=input_image,
