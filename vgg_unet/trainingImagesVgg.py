a28c6419adf7397e66a5e8e22242c5c49c706998
Andressa <dessaamaralcunha@gmail.com>
2021-03-25 12:41:22 -0300

    Teste 1 epoca 120 imagens

--- vgg_unet/trainingImagesVgg.py
+++ vgg_unet/trainingImagesVgg.py
@@ -7,7 +7,7 @@ def main():
 
 def learning():
     n_classes = 24
-    epochs = 30
+    epochs = 1
     model = vgg_unet(n_classes=n_classes, input_height=416, input_width=608)
     model.train( 
         train_images =  "original_images/",
